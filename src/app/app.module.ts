852ee39d052039eb7140d5e99b24eba53d597411
Vicente Correia <vicente@voxtecnologia.com.br>
2020-01-28 16:50:50 -0300

    Adicionando highcharts, bootstrap, toast e removendo conteúdo default do angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { ToastrModule } from 'ngx-toastr';
+import { ChartModule } from 'angular-highcharts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
